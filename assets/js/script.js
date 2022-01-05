111bbc3a25be65c67a3611060c1368babafb6f89
MariuszMatysiak <mariuszmatysiak2601@gmail.com>
2022-01-05 01:40:47 +0000

    Add script to the dungeons menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-
+// Hamburger Menu
 //  Code credit: https://www.youtube.com/watch?v=ydZc17rlR5E 
 const hamburger = document.getElementById('hamburger');
 const menuList = document.getElementById('menu-list');
@@ -6,3 +6,25 @@ hamburger.addEventListener('click', () => {
     menuList.classList.toggle('show');
 })
 // End of code credit 
+
+// Dungeons 
+// Code credit : https://stackhowto.com/how-to-get-hash-value-from-url-using-javascript/
+if(window.location.hash) {
+    let hash = window.location.hash.substring(1);
+    console.log(hash);
+    const dungeonContent = document.getElementById(`${hash}`);
+    dungeonContent.classList.toggle('show');
+}
+
+// }
+
+// const dungeonContent = document.getElementById("dungeon-content")
+// function mist() {
+//     const mistSection = document.getElementById("mist");
+//     if 
+//     mistSection.classList.toggle('show');
+// }
+// function dos() {
+//     const dosSection = document.getElementById("dos");
+//     dosSection.classList.toggle('show');
+// }
