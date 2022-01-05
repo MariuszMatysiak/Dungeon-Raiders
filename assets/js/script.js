1d24f49f5a7a173ed9ecbef38874492f7c6303bf
MariuszMatysiak <mariuszmatysiak2601@gmail.com>
2022-01-05 13:31:48 +0000

    Delete code from abandoned idea

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,25 +6,3 @@ hamburger.addEventListener('click', () => {
     menuList.classList.toggle('show');
 })
 // End of code credit 
-
-// Dungeons 
-// Code credit : https://stackhowto.com/how-to-get-hash-value-from-url-using-javascript/
-if(window.location.hash) {
-    let hash = window.location.hash.substring(1);
-    console.log(hash);
-    const dungeonContent = document.getElementById(`${hash}`);
-    dungeonContent.classList.toggle('show');
-}
-
-// }
-
-// const dungeonContent = document.getElementById("dungeon-content")
-// function mist() {
-//     const mistSection = document.getElementById("mist");
-//     if 
-//     mistSection.classList.toggle('show');
-// }
-// function dos() {
-//     const dosSection = document.getElementById("dos");
-//     dosSection.classList.toggle('show');
-// }
