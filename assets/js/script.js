5b55ffe4e9b8a7b6254de60e35991ffd3a3fe091
MariuszMatysiak <mariuszmatysiak2601@gmail.com>
2022-01-05 01:25:03 +0000

    Create JS file and update path file to it

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+//  Code credit: https://www.youtube.com/watch?v=ydZc17rlR5E 
+const hamburger = document.getElementById('hamburger');
+const menuList = document.getElementById('menu-list');
+hamburger.addEventListener('click', () => {
+    menuList.classList.toggle('show');
+})
+// End of code credit 
