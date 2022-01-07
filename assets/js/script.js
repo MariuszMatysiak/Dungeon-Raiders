68e124636b17186a21ad2dac59f1fb397362a9a4
MariuszMatysiak <mariuszmatysiak2601@gmail.com>
2022-01-07 01:58:01 +0000

    Add js code improving style of dungeon nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,24 @@ hamburger.addEventListener('click', () => {
     menuList.classList.toggle('show');
 })
 // End of code credit 
+
+// Code credit: https://stackhowto.com/how-to-get-hash-value-from-url-using-javascript/ for idea of using hash value, excution - Klaudia Matysiak
+/**
+ * Searches for previous active elements and removes its active class priviliges.
+ * Adds active class to the current link
+ */
+ const selectActiveLink = () => {
+    const previousActiveLinks = document.querySelectorAll('.dungeon-image.active');
+    previousActiveLinks.forEach((item) => {
+        item.classList.remove('active');
+    });
+    const activeLink = document.querySelector(`[href="${window.location.hash}"]`);
+    activeLink.classList.add('active');
+}
+
+if (window.location.hash) {
+    selectActiveLink();
+}
+
+// Listen for hash change 
+window.addEventListener('hashchange', selectActiveLink);
